2d9b5a5f0a5c494f6791f0b4654a0ac830f0943d
beleshivprasad <shivprasadoctomusprime@gmail.com>
2022-01-07 00:56:29 +0530

    UI done

--- client/src/screens/GamePage/GamePage.js
+++ client/src/screens/GamePage/GamePage.js
@@ -1,41 +1,62 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  Modal,
+  Row,
+} from "react-bootstrap";
 import axios from "axios";
 import "./GamePage.css";
+import { Link } from "react-router-dom";
 
 const GamePage = () => {
+  var wordSet = [];
   const [show, setShow] = useState(false);
   const [name, setName] = useState("");
-
+  const [started, setStarted] = useState(false);
+  const [lives, setLives] = useState(5);
+  const [score, setScore] = useState(0);
+  const [num, setNum] = useState(0);
+  const [word, setWord] = useState("");
+  const [checkWord, setCheckWord] = useState("");
+  localStorage.setItem("name", name);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [word, setWord] = useState("");
-  const [once, setOnce] = useState(0);
 
   const loadWord = async () => {
     const response = await axios.get(
       "https://random-word-api.herokuapp.com/word"
     );
     setWord(response.data);
+    let wd = Array.from(response.data[0]);
+    wordSet = wd;
+    // console.log(wordSet);
+    setNum(0 + Math.random() * (wd.length - 0));
   };
+  // console.log(num);
 
-  var renderField = (char, ind) => {
-    return (
-      <div className="field">
-        <Form.Control type="text" maxLength={1} />
-      </div>
-    );
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(word[0], checkWord);
+    if (word[0].normalize() === checkWord.normalize()) {
+      setScore(score + 1);
+      loadWord();
+    } else {
+      setLives(lives - 1);
+      loadWord();
+    }
   };
 
   return (
     <div className="gamepage">
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Player Name</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {" "}
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Control
@@ -47,31 +68,113 @@ const GamePage = () => {
                 }}
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
           </Form>
         </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Save
+          </Button>
+        </Modal.Footer>
       </Modal>
       <Container fluid className="game">
         <Row>
-          <Col className="col">
-            <Button variant="dark">Start</Button>
-          </Col>
-          <Col className="col">
-            <Button variant="dark">Start</Button>
-          </Col>
+          {score === 5 ? (
+            <Alert variant="warning">
+              Congratulation ...!! {name} You Won the Game
+            </Alert>
+          ) : (
+            <></>
+          )}
         </Row>
         <Row>
-          <Button variant="dark" onClick={handleShow}>
-            Start
+          {lives === 0 ? (
+            <Alert variant="danger">
+              Better luck next time ..!! You Lost the Game
+            </Alert>
+          ) : (
+            <></>
+          )}
+        </Row>
+        <Row className="gameArea">
+          <Col className="c">
+            <Button className="item" variant="dark">
+              Player {name !== "" ? name : ""}
+            </Button>
+          </Col>
+          <Col className="c">
+            <Button className="item" variant="warning">
+              Lives {lives}
+            </Button>
+          </Col>
+          <Col className="c">
+            <Button className="item" variant="success">
+              Score {score}
+            </Button>
+          </Col>
+        </Row>
+        <Row className="col"></Row>
+        <Row className="gameArea">
+          <Col className="c">
+            {started ? (
+              <Button
+                variant="dark"
+                className="item"
+                onClick={() => {
+                  localStorage.removeItem("name");
+                  setStarted(false);
+                  window.location.reload();
+                }}
+              >
+                <Link to="/start">End Game</Link>
+              </Button>
+            ) : (
+              <Button
+                variant="dark"
+                className="item"
+                onClick={() => {
+                  setStarted(true);
+                  if (name == "") {
+                    handleShow();
+                  }
+                  loadWord();
+                }}
+              >
+                Start Game
+              </Button>
+            )}
+          </Col>
+          <Col className="c">
+            <Button
+              variant="dark"
+              className="item"
+              onClick={() => {
+                localStorage.removeItem("name");
+                window.location.reload();
+              }}
+            >
+              <Link to="/start">New Game</Link>
+            </Button>
+          </Col>
+        </Row>
+        <Row className="gameAreamain word">
+          <span>{word}</span>
+        </Row>
+        <Row className="gameAreamain">
+          <Form>
+            <Form.Control
+              type="text"
+              placeholder="Enter Word"
+              value={checkWord}
+              onChange={(e) => {
+                setCheckWord(e.target.value);
+              }}
+            />
+          </Form>
+        </Row>
+        <Row className="gameArea">
+          <Button variant="primary" type="submit" onClick={submitHandler}>
+            Submit
           </Button>
-        </Row>
-        <Row>
-          <Form.Label htmlFor="inputPassword5">
-            Enter Character of Word {word}
-          </Form.Label>
-          {}
         </Row>
       </Container>
     </div>
